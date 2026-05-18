f65619c510d78b7c3d2c908566eac3cddc67878e
Iyad Mousa <bomussa@hotmail.com>
2026-05-18 07:15:46 +0300

    remove(pin): delete legacy pin verify endpoint

--- supabase/functions/pin-verify/index.ts
+++
@@ -1,9 +0,0 @@
-export default async function handler() {
-  return new Response(
-    JSON.stringify({
-      error: 'PIN_SYSTEM_REMOVED',
-      message: 'PIN system has been permanently disabled'
-    }),
-    { status: 410 }
-  );
-}
